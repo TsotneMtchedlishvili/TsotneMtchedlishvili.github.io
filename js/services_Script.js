528a34c2bba5ca8f86366899b64e4c8cb616d61d
Tsotne Mtchedlishvili <117540443+TsotneMtchedlishvili@users.noreply.github.com>
2025-05-14 00:16:46 +0400

    Update services js to now work for other boxes
    Makes it so that the services script now lets all the boxes be respon-
    sive instead of the first one.

--- js/services_Script.js
+++ js/services_Script.js
@@ -1,10 +1,18 @@
-const button = document.querySelector('.learn_More');
-const thumbnail = document.querySelector('.thumbnail');
+const button = Array.from(document.querySelectorAll('.learn_More'));
+const thumbnail = Array.from(document.querySelectorAll('.thumbnail'));
 
-button.addEventListener('mouseenter', () => {
-  thumbnail.style.transform = 'scale(1.2)';
+console.log(button);
+console.log(thumbnail);
+
+button.forEach((item) => {
+    item.addEventListener('mouseenter', () => {
+        thumbnail[button.indexOf(item)].style.transform = 'scale(1.2)';
+      });
 });
 
-button.addEventListener('mouseleave', () => {
-  thumbnail.style.transform = 'scale(1)';
-});
+button.forEach((item) => {
+    item.addEventListener('mouseleave', () => {
+        thumbnail[button.indexOf(item)].style.transform = 'scale(1)';
+      });
+})
+
