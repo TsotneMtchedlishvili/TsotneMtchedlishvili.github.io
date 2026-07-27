dc463a041ff606cd98eee984ff5032bbbffd7d15
Tsotne Mtchedlishvili <117540443+TsotneMtchedlishvili@users.noreply.github.com>
2026-07-27 17:43:02 +0400

    Bug fix wcu script to consider icon sizes too

--- js/whyChooseUsCardSizer.js
+++ js/whyChooseUsCardSizer.js
@@ -1,14 +1,16 @@
 const whyChooseCard = Array.from(document.querySelectorAll(".why_Choose_Card"));
+const icon = document.querySelector(".icon_Container")
+const iconSize = parseFloat(getComputedStyle(icon).height);
 
 const cardSizer = () => {
 
     let largestCardSize = 0;
+    const cardPadding = parseFloat(getComputedStyle(whyChooseCard[0]).paddingBlock);
 
     whyChooseCard.forEach((card) => {
 
         const titleHeight = card.querySelector(".why_Choose_Desc_Title").getBoundingClientRect().height;
         const descriptionHeight = card.querySelector(".why_Choose_Desc").getBoundingClientRect().height;
-        const cardPadding = parseFloat(getComputedStyle(card).paddingBlock);
         console.log(cardPadding)
         const cardSize = titleHeight + descriptionHeight + cardPadding * 2;
 
@@ -16,13 +18,26 @@ const cardSizer = () => {
 
     })
 
-    whyChooseCard.forEach((card) => {
+    console.log(iconSize)
+    console.log(cardPadding)
+    if(largestCardSize < iconSize) {
+        console.log("largestCardSize < iconSize")
+        largestCardSize = iconSize + cardPadding * 2;
+        whyChooseCard.forEach((card) => {
 
-        card.style.height = `${largestCardSize + (largestCardSize/100 * 5)}px`
+            card.style.height = `${largestCardSize}px`
         
-    })
+        })
+    } else {
+        console.log("largestCardSize > iconSize")
+        console.log(largestCardSize)
+        whyChooseCard.forEach((card) => {card.style.height = `${largestCardSize + (largestCardSize/100 * 5)}px`})
+        
+    }
+
+    
 }
 
-cardSizer();
+window.addEventListener("load", cardSizer)
 
 window.addEventListener("resize", cardSizer);
